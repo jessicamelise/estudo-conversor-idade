f05b5c0671b9bec8bcf5fa8ddf7e5869efc01c03
jessicamelise <61717333+jessicamelise@users.noreply.github.com>
2020-03-03 08:55:32 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,3 @@
+var age = parseInt (prompt ("Qual sua idade?"))
+var ageInSeconds = age * 365 * 24 * 60 * 60
+document.write("Em seus " + age + " anos de idade, se passaram " + ageInSeconds + " segundos.")
